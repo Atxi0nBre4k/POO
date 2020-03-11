8c058c620287aa1be33c6b11b66055a1c88dde67
AtxionBre4k <aerjorge01@gmail.com>
2020-03-10 18:57:57 -0700

    .

--- Actores/Program.cs
+++ Actores/Program.cs
@@ -4,31 +4,6 @@ namespace Pelicula
 {
     class Pelicula
     {
-        private string titulo;
-        private Int16 año;
-        private string pais;
-        private string director;
         
-        public Pelicula(string titulo, Int16 año)
-        {
-            this.titulo = titulo;
-            this.año = año;
-        }
-        public void imprime()
-        {
-            Console.WriteLine("{0},({1})",titulo,año);
-        }
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Pelicula p1 = new Pelicula("Baby Driver",2017);
-            p1.imprime();
-
-            Pelicula p2 = new Pelicula("Once Upon a Time in Hollywood", 2019);
-            p2.imprime();
-            
-        }
     }
 }
