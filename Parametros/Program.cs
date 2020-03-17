e2f8b007809e1bc89e1582fb8d0b0b3d1456114e
AtxionBre4k <aerjorge01@gmail.com>
2020-03-16 21:56:18 -0700

    Parametros2

--- Parametros/Program.cs
+++ Parametros/Program.cs
@@ -1,6 +1,57 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Parametros
 {
-    
+   class Persona
+   {
+       public string nombre;
+       public string apellido;
+
+       public Persona()
+       {
+           nombre = "Andres";
+           apellido = "Escalante";
+       }
+
+       class Anonimo
+       {
+           public static void Ocultar(Persona p)
+           {
+               p.nombre = "******";
+               p.apellido = "*********";
+           }
+
+           public static void Cambiar(ref Persona p)
+           {
+               p = new Persona();
+               p.nombre = "Nuevo";
+           }
+       }
+
+       class Program
+       {
+           static void Duplicador(int a)
+           {
+               a = a * 2;
+           }
+           static int Suma(int a, int b)
+           {
+               return a + b;
+           }
+       }
+       static void Main(string[] args)
+       {
+           int x = 2, y= 4;
+           int r = Suma(x,y);
+           int r2;
+           Suma(r,x, out r2);
+           Console.WriteLine(r2);
+
+           Persona p = new Persona();
+           Console.WriteLine(p.nombre);
+           Anonimo.Ocultar(p);
+           Console.WriteLine(p.nombre);
+
+       }
+   } 
 }
